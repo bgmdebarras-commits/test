d2af6443fcc263d5ea75109ff61ff445f2fb801c
bgmdebarras-commits <bgmdebarras@gmail.com>
2025-08-28 12:23:03 +0100

    email change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-yellow-500" />
-                <span>contact@bgm-debarras.fr</span>
+                <span>debarras.bgm@gmail.com</span>
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="w-5 h-5 text-yellow-500" />
